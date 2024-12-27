118fe6ef8cc01303c7d1afae5df7098343ada66c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-27 13:59:03 -0300

    Create PredictionRepository.java

---
+++ data-handler/src/main/java/tech/noetzold/data_handler/repository/PredictionRepository.java
@@ -0,0 +1,12 @@
+package tech.noetzold.data_handler.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.noetzold.data_handler.model.Prediction;
+
+import java.util.List;
+
+@Repository
+public interface PredictionRepository extends JpaRepository<Prediction, Long> {
+    List<Prediction> findBySensorName(String sensorName);
+}
