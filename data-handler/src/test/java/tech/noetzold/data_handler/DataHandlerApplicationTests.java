b71bcf78b42af9b9cbb7d09c81d4366e14ad002b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-26 22:55:09 -0300

    Create DataHandlerApplicationTests.java

---
+++ data-handler/src/test/java/tech/noetzold/data_handler/DataHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.data_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
