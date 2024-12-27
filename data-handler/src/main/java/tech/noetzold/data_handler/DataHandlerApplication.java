7d0e14ab326b9b149e69bace96c151d5ef0c1288
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-26 22:55:17 -0300

    Create DataHandlerApplication.java

---
+++ data-handler/src/main/java/tech/noetzold/data_handler/DataHandlerApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.data_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataHandlerApplication.class, args);
+	}
+
+}
