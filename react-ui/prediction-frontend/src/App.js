9cad976a963560dde1c85f13e178f3e1de250e45
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-27 22:53:20 -0300

    Create App.js

---
+++ react-ui/prediction-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ConfigForm from "./components/ConfigForm"; // Certifique-se de que o caminho para ConfigForm está correto.
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simulador de Predições</h1>
+      <ConfigForm />
+    </div>
+  );
+}
+
+export default App;
