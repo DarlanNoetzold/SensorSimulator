4fbb6dcfa714a76e9c4d3ac44b0464384e65785a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-27 22:54:03 -0300

    Update DataService.java

--- data-handler/src/main/java/tech/noetzold/data_handler/service/DataService.java
+++ data-handler/src/main/java/tech/noetzold/data_handler/service/DataService.java
@@ -1,18 +1,20 @@
 package tech.noetzold.data_handler.service;
 
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import tech.noetzold.data_handler.model.Prediction;
 import tech.noetzold.data_handler.repository.PredictionRepository;
 
 import java.util.List;
 
-@Service
+@Component
 public class DataService {
 
-    private final PredictionRepository predictionRepository;
+    @Autowired
+    private PredictionRepository predictionRepository;
 
-    public DataService(PredictionRepository predictionRepository) {
-        this.predictionRepository = predictionRepository;
+    public DataService() {
+
     }
 
     public List<Prediction> getAllPredictions() {
