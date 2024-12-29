718dfa56d3443580f3509aa2d3fc846989138059
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 07:30:53 -0300

    Update DataHandlerApplication.java

--- data-handler/src/main/java/tech/noetzold/data_handler/DataHandlerApplication.java
+++ data-handler/src/main/java/tech/noetzold/data_handler/DataHandlerApplication.java
@@ -2,6 +2,8 @@ package tech.noetzold.data_handler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class DataHandlerApplication {
