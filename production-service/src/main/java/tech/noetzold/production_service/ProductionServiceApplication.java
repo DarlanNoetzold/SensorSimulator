e9de6f3a22f7bf4da829120b17cf676da62f63f0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 10:21:19 -0300

    Create ProductionServiceApplication.java

---
+++ production-service/src/main/java/tech/noetzold/production_service/ProductionServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.production_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionServiceApplication.class, args);
+	}
+
+}
