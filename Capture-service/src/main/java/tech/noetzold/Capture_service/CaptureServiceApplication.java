99543360f096cc4500169dd0071ad9abf04a5fcc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 15:19:58 -0300

    Create CaptureServiceApplication.java

---
+++ Capture-service/src/main/java/tech/noetzold/Capture_service/CaptureServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.Capture_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureServiceApplication.class, args);
+	}
+
+}
