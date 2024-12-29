76084e8d6ca6098e7f2dd59d56648a479a068af1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 15:54:00 -0300

    Create GatewayCapturerApplication.java

---
+++ gateway-capturer/src/main/java/tech/noetzold/gateway_capturer/GatewayCapturerApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.gateway_capturer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayCapturerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayCapturerApplication.class, args);
+	}
+
+}
