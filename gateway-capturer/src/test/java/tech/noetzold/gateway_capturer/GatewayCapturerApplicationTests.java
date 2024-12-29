b5e3e3e83710367d07e7dfdbd418fcf34f0d7123
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 15:54:07 -0300

    Create GatewayCapturerApplicationTests.java

---
+++ gateway-capturer/src/test/java/tech/noetzold/gateway_capturer/GatewayCapturerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.gateway_capturer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayCapturerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
