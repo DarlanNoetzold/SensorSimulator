55ed0782455e57b4cc7c9c5ebb560b684e8082b0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-29 15:19:47 -0300

    Update ProductionService.java

--- production-service/src/main/java/tech/noetzold/production_service/service/ProductionService.java
+++ production-service/src/main/java/tech/noetzold/production_service/service/ProductionService.java
@@ -1,5 +1,6 @@
 package tech.noetzold.production_service.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,15 +17,30 @@ public class ProductionService {
     @Autowired
     private PredictionRepository predictionRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper; // Usado para serializar o objeto Prediction em JSON
+
     // Enviar predições a cada 5 minutos
     @Scheduled(fixedRate = 300000) // 5 minutos em milissegundos
     public void sendPredictionsToQueue() {
         Iterable<Prediction> predictions = predictionRepository.findAll();
 
         for (Prediction prediction : predictions) {
-            // Enviar cada predição para a fila RabbitMQ
-            rabbitTemplate.convertAndSend("productionQueue", prediction);
-            System.out.println("Sent prediction: " + prediction.getSensorName());
+            try {
+                // Serializar o objeto Prediction para JSON
+                String jsonPrediction = objectMapper.writeValueAsString(prediction);
+
+                // Enviar a mensagem JSON para a fila RabbitMQ
+                rabbitTemplate.convertAndSend("productionQueue", jsonPrediction);
+                System.out.println("Sent prediction: " + prediction.getSensorName());
+
+                // Remover a previsão do banco de dados após enviá-la para a fila
+                predictionRepository.delete(prediction);
+                System.out.println("Deleted prediction: " + prediction.getSensorName());
+
+            } catch (Exception e) {
+                System.err.println("Error serializing and sending prediction: " + e.getMessage());
+            }
         }
     }
 }
