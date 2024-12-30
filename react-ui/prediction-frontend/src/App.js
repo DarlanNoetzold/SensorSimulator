61db4787cf34a3b995dce8006ae7d9238bc423e5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-30 13:57:21 -0300

    Update App.js

--- react-ui/prediction-frontend/src/App.js
+++ react-ui/prediction-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ConfigForm from "./components/ConfigForm";
 import SensorPredictionSearch from './components/SensorPredictionSearch';
+import SendPredictions from './components/SendPredictions';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <ConfigForm />
       <h1>Sensor Predictions</h1>
       <SensorPredictionSearch />
+      <h1>Send Predictions</h1>
+      <SendPredictions />
     </div>
   );
 }
