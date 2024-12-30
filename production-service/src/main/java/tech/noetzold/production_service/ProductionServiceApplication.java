d3425d1498b17b67add0c4ea0bfbe3178f5e168a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-30 15:07:12 -0300

    Update ProductionServiceApplication.java

--- production-service/src/main/java/tech/noetzold/production_service/ProductionServiceApplication.java
+++ production-service/src/main/java/tech/noetzold/production_service/ProductionServiceApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.production_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProductionServiceApplication {
 
 	public static void main(String[] args) {
