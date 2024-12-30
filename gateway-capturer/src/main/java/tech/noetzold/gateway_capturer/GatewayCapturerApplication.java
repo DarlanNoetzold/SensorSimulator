88b1078ec79179bbd90f281cb8b46416c28aded4
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-30 15:07:06 -0300

    Update GatewayCapturerApplication.java

--- gateway-capturer/src/main/java/tech/noetzold/gateway_capturer/GatewayCapturerApplication.java
+++ gateway-capturer/src/main/java/tech/noetzold/gateway_capturer/GatewayCapturerApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.gateway_capturer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GatewayCapturerApplication {
 
 	public static void main(String[] args) {
