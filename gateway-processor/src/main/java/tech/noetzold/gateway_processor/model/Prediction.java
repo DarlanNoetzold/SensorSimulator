b3da496c81a1d599ce5154f5692742b1c42edf33
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-12-31 11:19:24 -0300

    Create Prediction.java

---
+++ gateway-processor/src/main/java/tech/noetzold/gateway_processor/model/Prediction.java
@@ -0,0 +1,81 @@
+package tech.noetzold.gateway_processor.model;
+
+import java.time.LocalDateTime;
+
+public class Prediction {
+
+    private Long id;
+
+    private String sensorName;
+
+    private String unit;
+
+    private Integer interval;
+
+    private LocalDateTime predictedDate;
+
+    private Double value;
+
+    public Prediction() {
+    }
+
+    public Prediction(Long id, String sensorName, String unit, Integer interval, LocalDateTime predictedDate, Double value) {
+        this.id = id;
+        this.sensorName = sensorName;
+        this.unit = unit;
+        this.interval = interval;
+        this.predictedDate = predictedDate;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSensorName() {
+        return sensorName;
+    }
+
+    public void setSensorName(String sensorName) {
+        this.sensorName = sensorName;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getInterval() {
+        return interval;
+    }
+
+    public void setInterval(Integer interval) {
+        this.interval = interval;
+    }
+
+    public LocalDateTime getPredictedDate() {
+        return predictedDate;
+    }
+
+    public void setPredictedDate(LocalDateTime predictedDate) {
+        this.predictedDate = predictedDate;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+
+
+}
