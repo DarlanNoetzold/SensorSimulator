a43c2f5aca4d7daf6ed027747b46b8f099ccd73d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-01 16:01:30 -0300

    Update Metrics.java

--- processor-service/src/main/java/tech/noetzold/processor_service/model/Metrics.java
+++ processor-service/src/main/java/tech/noetzold/processor_service/model/Metrics.java
@@ -1,12 +1,23 @@
 package tech.noetzold.processor_service.model;
 
+import jakarta.persistence.*;
+
 import java.util.Map;
 
+@Entity
 public class Metrics {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private double cpuUsage;
     private long memoryUsage;
     private int threadCount;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "sensor_variance", joinColumns = @JoinColumn(name = "metrics_id"))
+    @MapKeyColumn(name = "sensor_type")
+    @Column(name = "variance")
     private Map<String, Double> varianceMap;
     private long totalDataReceived;
     private long totalDataFiltered;
