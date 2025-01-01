0f78b1cff703515ce92059f4829f63a87587ad87
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-01 16:22:29 -0300

    Create build.bat

---
+++ processor-service/c/build.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+:: Configurar caminho do JDK
+set JAVA_HOME=C:\Program Files\Java\jdk-22
+
+:: Verificar se JAVA_HOME está configurado corretamente
+if not exist "%JAVA_HOME%\bin\javac.exe" (
+    echo JAVA_HOME is not set correctly.
+    exit /b 1
+)
+
+:: Compilar data_filter.c
+gcc -shared -o libdata_filter.dll -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" data_filter.c
+
+:: Compilar data_compression.c
+gcc -shared -o libdata_compression.dll -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" data_compression.c
+
+:: Compilar data_aggregation.c
+gcc -shared -o libdata_aggregation.dll -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" data_aggregation.c
+
+endlocal
+echo Compilation complete.
