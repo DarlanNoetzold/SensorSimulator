fa6ba9ea9caf25e7226676178212073e3005e520
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-03 08:52:04 -0300

    Update ProcessorServiceApplication.java

--- processor-service/src/main/java/tech/noetzold/processor_service/ProcessorServiceApplication.java
+++ processor-service/src/main/java/tech/noetzold/processor_service/ProcessorServiceApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.processor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProcessorServiceApplication {
 
 	public static void main(String[] args) {
