e33075173c16dbb7d7f6f91b396dfaf6b5df0ff1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-03 18:38:39 -0300

    Update MetricsController.java

--- core-service/src/main/java/tech/noetzold/core_service/controller/MetricsController.java
+++ core-service/src/main/java/tech/noetzold/core_service/controller/MetricsController.java
@@ -1,6 +1,8 @@
 package tech.noetzold.core_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.core_service.model.Metrics;
@@ -8,8 +10,10 @@ import tech.noetzold.core_service.repository.MetricsRepository;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/api/metrics")
 public class MetricsController {
+
     @Autowired
     private MetricsRepository metricsRepository;
 
@@ -20,11 +24,16 @@ public class MetricsController {
         return ResponseEntity.ok(metrics);
     }
 
-    // Endpoint para buscar as últimas X métricas
+    // Endpoint para buscar as últimas métricas com um limite
     @GetMapping("/latest")
     public ResponseEntity<List<Metrics>> getLatestMetrics(
             @RequestParam(value = "limit", defaultValue = "10") int limit) {
-        List<Metrics> metrics = metricsRepository.findTopByOrderByIdDesc(limit);
+
+        // Aplica o limite no Pageable
+        Pageable pageable = PageRequest.of(0, limit);
+
+        // Busca as últimas métricas
+        List<Metrics> metrics = metricsRepository.findAll(pageable).getContent();
         return ResponseEntity.ok(metrics);
     }
 }
