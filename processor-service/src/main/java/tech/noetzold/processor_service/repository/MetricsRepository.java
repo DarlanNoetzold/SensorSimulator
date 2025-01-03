72e40ab3cbb1043dd0e52b6753327089159fce5b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-03 15:17:30 -0300

    Update MetricsRepository.java

--- processor-service/src/main/java/tech/noetzold/processor_service/repository/MetricsRepository.java
+++ processor-service/src/main/java/tech/noetzold/processor_service/repository/MetricsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 import tech.noetzold.processor_service.model.Metrics;
 
 @Repository
-public interface MetricsRepository  extends JpaRepository<Metrics, Long> {
+public interface MetricsRepository extends JpaRepository<Metrics, Long> {
 }
