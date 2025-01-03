8b745ce52aad67a798e663f6dc0b3ade89004872
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-03 18:38:35 -0300

    Create RabbitMQConfig.java

---
+++ core-service/src/main/java/tech/noetzold/core_service/config/RabbitMQConfig.java
@@ -0,0 +1,15 @@
+package tech.noetzold.core_service.config;
+
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tech.noetzold.core_service.service.MetricsConsumerService;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+}
