70894fce103dbf08cda21845c8ecbde1272b91de
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-03 08:49:49 -0300

    Update SensorRecordRepository.java

--- core-service/src/main/java/tech/noetzold/core_service/repository/SensorRecordRepository.java
+++ core-service/src/main/java/tech/noetzold/core_service/repository/SensorRecordRepository.java
@@ -3,5 +3,13 @@ package tech.noetzold.core_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.core_service.model.SensorRecord;
 
+import java.util.List;
+
 public interface SensorRecordRepository extends JpaRepository<SensorRecord, Long> {
+
+    // Método para buscar os dados de um sensor específico, limitados e ordenados pela data (do mais recente para o mais antigo)
+    List<SensorRecord> findTopBySensorNameOrderByPredictedDateDesc(String sensorName, int limit);
+
+    // Método para buscar todos os dados, limitados e ordenados pela data (do mais recente para o mais antigo)
+    List<SensorRecord> findTopByOrderByPredictedDateDesc(int limit);
 }
