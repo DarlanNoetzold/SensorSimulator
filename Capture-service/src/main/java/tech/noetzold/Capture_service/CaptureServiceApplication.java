98d19874f2958c1caf41fd9e90204cfcc0ad2953
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-01-04 15:13:55 -0300

    Update CaptureServiceApplication.java

--- Capture-service/src/main/java/tech/noetzold/Capture_service/CaptureServiceApplication.java
+++ Capture-service/src/main/java/tech/noetzold/Capture_service/CaptureServiceApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.Capture_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CaptureServiceApplication {
 
 	public static void main(String[] args) {
