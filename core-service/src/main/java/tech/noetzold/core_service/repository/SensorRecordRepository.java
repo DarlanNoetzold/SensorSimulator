ed898eebfbdde40dd53d82ddbf93918f6aed05a7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-27 13:57:02 -0300

    Update SensorRecordRepository.java

--- core-service/src/main/java/tech/noetzold/core_service/repository/SensorRecordRepository.java
+++ core-service/src/main/java/tech/noetzold/core_service/repository/SensorRecordRepository.java
@@ -12,10 +12,8 @@ import java.util.List;
 @Repository
 public interface SensorRecordRepository extends JpaRepository<SensorRecord, Long> {
 
-    // Método para buscar os dados de um sensor específico, limitados e ordenados pela data (do mais recente para o mais antigo)
     Page<SensorRecord> findBySensorNameOrderByPredictedDateDesc(String sensorName, Pageable pageable);
 
-    // Método para buscar todos os dados, limitados e ordenados pela data (do mais recente para o mais antigo)
     Page<SensorRecord> findAllByOrderByPredictedDateDesc(Pageable pageable);
 }
 
