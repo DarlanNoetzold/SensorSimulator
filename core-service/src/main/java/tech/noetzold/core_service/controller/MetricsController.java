912e9b5497dfed3a4ffa0b6c8663035aea1c7896
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-27 13:56:58 -0300

    Update MetricsController.java

--- core-service/src/main/java/tech/noetzold/core_service/controller/MetricsController.java
+++ core-service/src/main/java/tech/noetzold/core_service/controller/MetricsController.java
@@ -17,22 +17,18 @@ public class MetricsController {
     @Autowired
     private MetricsRepository metricsRepository;
 
-    // Endpoint para buscar todas as métricas
     @GetMapping
     public ResponseEntity<List<Metrics>> getAllMetrics() {
         List<Metrics> metrics = metricsRepository.findAll();
         return ResponseEntity.ok(metrics);
     }
 
-    // Endpoint para buscar as últimas métricas com um limite
     @GetMapping("/latest")
     public ResponseEntity<List<Metrics>> getLatestMetrics(
             @RequestParam(value = "limit", defaultValue = "10") int limit) {
 
-        // Aplica o limite no Pageable
         Pageable pageable = PageRequest.of(0, limit);
 
-        // Busca as últimas métricas
         List<Metrics> metrics = metricsRepository.findAll(pageable).getContent();
         return ResponseEntity.ok(metrics);
     }
