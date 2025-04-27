b5af36dbcf19e1ebdeab8edb91f73d98fea48068
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2025-04-27 13:57:04 -0300

    Update MetricsConsumerService.java

--- core-service/src/main/java/tech/noetzold/core_service/service/MetricsConsumerService.java
+++ core-service/src/main/java/tech/noetzold/core_service/service/MetricsConsumerService.java
@@ -23,21 +23,17 @@ public class MetricsConsumerService {
 
     // Método para processar as mensagens da fila de métricas
     @RabbitListener(queues = "metrics")
-    @Transactional  // Garantindo que a operação de salvar no banco seja feita dentro de uma transação
+    @Transactional
     public void consumeMetricsMessage(String payload) {
         try {
-            // Deserializar a mensagem JSON para um Map
             Map<String, Object> metricsData = objectMapper.readValue(payload, Map.class);
 
-            // Converter o Map para o objeto Metrics
             Metrics metrics = convertMapToMetrics(metricsData);
 
-            // Salvar no banco de dados
             metricsRepository.save(metrics);
 
             System.out.println("Metrics saved successfully: " + metrics);
         } catch (IOException e) {
-            // Logando erro e rejeitando a mensagem sem reprocessar
             System.err.println("Error while deserializing message: " + e.getMessage());
             throw new org.springframework.amqp.AmqpRejectAndDontRequeueException("Message format invalid", e);
         }
@@ -48,22 +44,18 @@ public class MetricsConsumerService {
         Metrics metrics = new Metrics();
         metrics.setCpuUsage((Double) metricsData.get("cpuUsage"));
 
-        // Convertendo explicitamente para Long, caso o valor seja Integer
         metrics.setMemoryUsage(((Integer) metricsData.get("memoryUsage")).longValue());
 
         metrics.setThreadCount((Integer) metricsData.get("threadCount"));
 
-        // Variance Map pode ser um Map de Strings para Double, então deve ser mantido
         metrics.setVarianceMap((Map<String, Double>) metricsData.get("varianceMap"));
 
-        // Convertendo explicitamente para Long
         metrics.setTotalDataReceived(((Integer) metricsData.get("totalDataReceived")).longValue());
         metrics.setTotalDataFiltered(((Integer) metricsData.get("totalDataFiltered")).longValue());
         metrics.setTotalDataCompressed(((Integer) metricsData.get("totalDataCompressed")).longValue());
         metrics.setTotalDataAggregated(((Integer) metricsData.get("totalDataAggregated")).longValue());
         metrics.setTotalDataAfterHeuristics(((Integer) metricsData.get("totalDataAfterHeuristics")).longValue());
 
-        // Convertendo o valor para Long
         metrics.setErrorCount(((Integer) metricsData.get("errorCount")).longValue());
 
         metrics.setProcessorId((String) metricsData.get("processorId"));
